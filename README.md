eb3bd3060f993916632c55db56e69f1c7146ab30
KD76 <KD76@users.noreply.github.com>
2015-09-24 10:54:41 +0200

    Update README.md

---
+++ README.md
@@ -0,0 +1,22 @@
+# Info 2A
+Vous retrouverez ici tous les TP d'informatique de 2ème année de l'INSA de Lyon
+
+### Préambule
+Afin de faciliter la compréhension de ce code, celui-ci sera entièrement commenté (c'est à dire, **ligne par ligne**).
+Pour les utilisateurs confirmés, une version non commentée sera disponible sur une branche secondaire.
+
+### Prérequis
+Comprendre Java, les notions d'objets, les méthodes statiques, ainsi que l'héritage et les propriétés qui en découlent.
+
+Ces classes sont à la base des TP de deuxième année de l'INSA de Lyon.
+
+### Compilation et utilisation
+Ce hub ne sera pas pourvu de fichiers compilés, ou du moins pas pour le moment. Cela sera fera peut-être en tout fin d'année afin de pouvoir choisir quel programme lancer parmi tous les TP.
+
+Afin de compiler les fichiers, merci de vous rendre dans le dossier src, puis le dossier du TP en question puis compiler les fichiers à l'aide de la commande `javac`
+
+### Droits et limites d'usage
+Ce code reste la propriété des étudiants de l'INSA de Lyon et est autorisé son usage à titre pédagogique ou personnel.
+
+### Questions, remarques ?
+N'hésitez surtout pas ! On est d'abord là pour apprendre, chaque commentaire est le bienvenu ;)
--- src/Polynomes/Polynome.java
+++ src/Polynomes/Polynome.java
@@ -1,93 +1,93 @@
 package Polynomes;
 
 class Polynome {
-	
-	private double[] coeffs;
+
+    private double[] coeffs;
     private Polynome primitive;
     private Polynome derivee;
-	
-	public Polynome(double[] c) {
-		coeffs = new double[c.length];
-		System.arraycopy(c, 0, coeffs, 0, c.length);
-	}
-	
-	public String toString() {
-		String data = "";
-		for(int i=coeffs.length-1;i>0;i--) {
-			data += " ("+coeffs[i]+")";
-			data += " x^"+i+" +";
-		}
-		data += " ("+coeffs[0]+")";
-		return data;
-	}
-	
-	public double valPoly(double x) {
-		double val=0;
-		for(int i=0; i<coeffs.length; i++) {
-			val += coeffs[i]*Math.pow(x, i);
-		}
-		return val;
-	}
-	
-	public double horner(double x) {
-		
-		double val = 0;
-		
-		for(int i=coeffs.length-1; i>0; i--) {
-			val += coeffs[i];
-			val *= x;
-		}
-		
-		val += coeffs[0];
-		
-		return val;
-	}
-	
-	public Polynome derivePoly() {
+
+    public Polynome(double[] c) {
+        coeffs = new double[c.length];
+        System.arraycopy(c, 0, coeffs, 0, c.length);
+    }
+
+    public String toString() {
+        String data = "";
+        for (int i = coeffs.length - 1; i > 0; i--) {
+            data += " (" + coeffs[i] + ")";
+            data += " x^" + i + " +";
+        }
+        data += " (" + coeffs[0] + ")";
+        return data;
+    }
+
+    public double valPoly(double x) {
+        double val = 0;
+        for (int i = 0; i < coeffs.length; i++) {
+            val += coeffs[i] * Math.pow(x, i);
+        }
+        return val;
+    }
+
+    public double horner(double x) {
+
+        double val = 0;
+
+        for (int i = coeffs.length - 1; i > 0; i--) {
+            val += coeffs[i];
+            val *= x;
+        }
+
+        val += coeffs[0];
+
+        return val;
+    }
+
+    public Polynome derivePoly() {
         if (derivee == null) {
-            double[] derivCoeffs = new double[coeffs.length -1]; // Suppression d'un degré.
-            for(int i=coeffs.length-1;i>=1; i--) {
-                derivCoeffs[i-1] = coeffs[i]*i;
+            double[] derivCoeffs = new double[coeffs.length - 1]; // Suppression d'un degré.
+            for (int i = coeffs.length - 1; i >= 1; i--) {
+                derivCoeffs[i - 1] = coeffs[i] * i;
             }
             derivee = new Polynome(derivCoeffs);
         }
         return derivee;
-	}
-	
-	public double polyNewton(double start, int precision) {
-		Polynome derivPoly = derivePoly();
-		double tenPrecision = Math.pow(10, -1*precision);
-		
-		
-		double solution = start;
-		double oldVal;
-		do {
-			oldVal = solution;
-			solution = oldVal - valPoly(oldVal)/derivPoly.valPoly(oldVal);
-		} while(Math.abs(solution-oldVal) > tenPrecision);
-		
-		return solution;
-	}
+    }
 
-	public double integrerRectangle(double a, double b, int N) {
-		double delta = (b-a)/(double)N;
-		double val = 0;
+    public double polyNewton(double start, int precision) {
+        Polynome derivPoly = derivePoly();
+        double tenPrecision = Math.pow(10, -1 * precision);
 
-		for (int i=0; i<N; i++) {
-			val += horner((0.5+i)*delta+a);
-		}
 
-		val *= delta;
+        double solution = start;
+        double oldVal;
+        do {
+            oldVal = solution;
+            solution = oldVal - valPoly(oldVal) / derivPoly.valPoly(oldVal);
+        } while (Math.abs(solution - oldVal) > tenPrecision);
 
-		return val;
-	}
+        return solution;
+    }
+
+    public double integrerRectangle(double a, double b, int N) {
+        double delta = (b - a) / (double) N;
+        double val = 0;
+
+        for (int i = 0; i < N; i++) {
+            val += horner((0.5 + i) * delta + a);
+        }
+
+        val *= delta;
+
+        return val;
+    }
 
     public Polynome primitivePoly() {
         if (this.primitive == null) {
-            double[] primitive = new double[coeffs.length+1];
+            double[] primitive = new double[coeffs.length + 1];
 
-            for(int i=0; i<coeffs.length; i++) {
-                primitive[i+1] = coeffs[i]/(i+1);
+            for (int i = 0; i < coeffs.length; i++) {
+                primitive[i + 1] = coeffs[i] / (i + 1);
             }
             primitive[0] = 0;
 
@@ -99,28 +99,28 @@ class Polynome {
     public double integrerPoly(double a, double b) {
         Polynome prim = primitivePoly();
 
-        return prim.horner(b)-prim.horner(a);
+        return prim.horner(b) - prim.horner(a);
     }
 
-	public static void main(String[] args) {
-		double[] tab = {-6, -8, 1, 4, 1};
-		Polynome pol = new Polynome(tab);
-		System.out.println(pol);
-		double val1 = pol.polyNewton(-4, 7);
-		double val2 = pol.polyNewton(-2, 15);
-		double val3 = pol.polyNewton(0, 15);
-		double val4 = pol.polyNewton(2, 15);
-		System.out.println(val1);
-		System.out.println(val2);
-		System.out.println(val3);
-		System.out.println(val4);
+    public static void main(String[] args) {
+        double[] tab = {-6, -8, 1, 4, 1};
+        Polynome pol = new Polynome(tab);
+        System.out.println(pol);
+        double val1 = pol.polyNewton(-4, 7);
+        double val2 = pol.polyNewton(-2, 15);
+        double val3 = pol.polyNewton(0, 15);
+        double val4 = pol.polyNewton(2, 15);
+        System.out.println(val1);
+        System.out.println(val2);
+        System.out.println(val3);
+        System.out.println(val4);
 
-		System.out.println("f(5) = "+pol.horner(5));
+        System.out.println("f(5) = " + pol.horner(5));
 
-        System.out.println("Intégration de f sur [0,1] avec N = 20 : "+pol.integrerRectangle(0, 1, 600));
-        System.out.println("Une primitive du polynome: "+pol.primitivePoly());
-        System.out.println("Valeur exacte de l'intégrale: "+pol.integrerPoly(0,1));
+        System.out.println("Intégration de f sur [0,1] avec N = 20 : " + pol.integrerRectangle(0, 1, 600));
+        System.out.println("Une primitive du polynome: " + pol.primitivePoly());
+        System.out.println("Valeur exacte de l'intégrale: " + pol.integrerPoly(0, 1));
 
-	}
-	
+    }
+
 }
